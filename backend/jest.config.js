29c5211fe009de1d7661dcfb570d42d632b4f9b5
Nethra Nayak <nethra.nayak23@spit.ac.in>
2026-05-08 03:56:53 +0530

    test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,25 @@
 export default {
   testEnvironment: "node",
+
   transform: {
     "^.+\\.js$": "babel-jest",
   },
+
   testMatch: ["**/src/tests/unit/**/*.test.js"],
+
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/tests/**",
+    "!**/node_modules/**"
+  ],
+
+  coverageDirectory: "coverage",
+
+  coverageReporters: ["text", "lcov"],
+
   forceExit: true,
+
   testTimeout: 30000,
 };
